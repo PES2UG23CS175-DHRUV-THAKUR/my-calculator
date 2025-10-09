b875dc91e9e2fbfc656afd159470f96e800dd47f
PES2UG23CS175-DHRUV-THAKUR <pes2ug23cs175@pesu.pes.edu>
2025-10-09 14:08:19 +0530

    - Deploy added to pipeline - Completed the CI-CD pipeline implementation completely

--- tests/integration/test_cli_integration.py
+++ tests/integration/test_cli_integration.py
@@ -89,7 +89,7 @@ class TestCLIIntegration:
     def test_cli_sqrt_integration(self):
         """Test CLI can perform square root"""
         res = self.run_cli('sqrt', '16')
-        assert res.exit_code == 0
+        assert res.returncode == 0 # Changed from exit_code
         
         # FIX: Check only the last line of stdout to ignore verbose output
         output_lines = res.output.strip().split('\n')
